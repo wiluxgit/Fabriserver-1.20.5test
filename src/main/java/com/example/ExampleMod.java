49ec9c2ebf724454c066fe52e3d7277b479d4403
Oscar Dahlqvist <oscdah@student.chalmers.se>
2024-04-06 16:55:10 +0200

    fist working example of inserting items into slot

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,7 +1,25 @@
 package com.example;
 
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registry;
+import net.minecraft.screen.*;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,7 +34,40 @@ public class ExampleMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(
+				net.minecraft.server.command.CommandManager.literal("foo").executes(this::foo)
+		));
+	}
+	public int foo(CommandContext<ServerCommandSource> context) {
+		context.getSource().sendFeedback(() -> Text.literal("hello world"), false);
+
+		ServerPlayerEntity splayer = context.getSource().getPlayer();
+		var screenHandler = new SimpleNamedScreenHandlerFactory(
+				(syncId, playerInventory, player) -> new MyStonecutterScreenHandler(0, player.getInventory()),
+				Text.literal("Title")
+		);
+		splayer.openHandledScreen(screenHandler);
+		splayer.currentScreenHandler.setStackInSlot(0, 0, new ItemStack(Items.DIAMOND_SWORD));
+
+		return 1;
+	}
+	public class MyStonecutterScreenHandler extends StonecutterScreenHandler {
+		public MyStonecutterScreenHandler(int syncId, PlayerInventory playerInventory) {
+			super(syncId, playerInventory);
+		}
+
+		@Override
+		public void updateSyncHandler(ScreenHandlerSyncHandler screenHandler) {
+			super.updateSyncHandler(screenHandler);
+			System.out.println(">>>>>>>>>>>>> updateSyncHandler");
+		}
+
+		@Override
+		public void addListener(ScreenHandlerListener screenHandler) {
+			super.addListener(screenHandler);
+			System.out.println(">>>>>>>>>>>>> addListener");
+		}
 	}
 }
